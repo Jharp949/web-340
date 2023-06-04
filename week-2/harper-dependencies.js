3a707d6f3930e379b1687f71ba2d0756660900f8
James Harper <jharp949@gmail.com>
2023-06-04 15:31:59 -0700

    Changes to be committed: 	new file:   week-2/harper-dependencies.js 	deleted:    week-2/harper-palindrome-app.js

---
+++ week-2/harper-dependencies.js
@@ -0,0 +1,4 @@
+//Title: harper-dependencies.js
+//Author: James Harper
+//Date: 6/3/2023
+//Description: Web 340 - Assignment 2.2
--- week-2/harper-palindrome-app.js
+++
@@ -1,4 +0,0 @@
-//Title: harper-palindrome-app.js
-//Author: James Harper
-//Date: 5/12/2023
-//Description: Web 231 - Week 9 - Assignment 9.2
