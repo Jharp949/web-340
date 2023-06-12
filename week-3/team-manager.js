50551fdb4c9f669b181d1931e1fb4014741cee31
James Harper <jharp949@gmail.com>
2023-06-11 18:43:22 -0700

    Changes to be committed: 	new file:   week-3/index.js 	new file:   week-3/package-lock.json 	new file:   week-3/package.json 	new file:   week-3/team-manager.js 	modified:   week-3/team.js

---
+++ week-3/team-manager.js
@@ -0,0 +1,17 @@
+//Title: team-manager.js
+//Author: James Harper
+//Date: 6/10/2023
+//Description: Web 340 - Assignment 3.2
+
+"use strict";
+
+const Team = require('./team')
+
+const teams = [
+  new Team(),
+  new Team(),
+  new Team(),
+  new Team(),
+  new Team(),
+];
+
--- week-3/team.js
+++ week-3/team.js
@@ -1,5 +1,16 @@
-//Title: harper-team.js
+//Title: team.js
 //Author: James Harper
 //Date: 6/10/2023
 //Description: Web 340 - Assignment 3.2
 
+"use strict";
+
+class Team {
+  constructor(name, mascot, playerCount) {
+    this.name = name;
+    this.mascot = mascot;
+    this.playerCount = playerCount;
+  }
+}
+
+module.exports = Team;
